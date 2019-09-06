622965fbabd146c81ca7d40bd06f2a4bba67d524
torageballo <torage.ballo@unacast.com>
2019-09-06 15:20:41 +0200

    WIP, added functionality for displaying background rewards dialog. But we are not counting correct yet

---
+++ ClickerExample/Scripts/BackgroundLocationReward.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Unaty.PureSDK;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BackgroundLocationReward : MonoBehaviour, IPointerDownHandler
+{
+    public GameState state;
+    public PureSDKComponent tracking;
+
+    private void Start()
+    {
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus)
+        {
+            if (tracking.IsTracking() && state.secondsSincePreviousPlaySession > 0)
+            {
+                Debug.Log("Amount: " + state.secondsSincePreviousPlaySession);
+                gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void Upgrade()
+    {
+        Debug.Log("Upgrade");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
