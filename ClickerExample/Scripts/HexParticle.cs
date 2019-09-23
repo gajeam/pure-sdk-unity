eef1c9e2a31bf6bdc1c4284d1f4bd2552d33139d
torageballo <torage.ballo@unacast.com>
2019-09-23 13:12:10 +0200

    Added Skybox. Swapto diamond

---
+++ ClickerExample/Scripts/HexParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexParticle : MonoBehaviour
+{
+    public GameObject swapsTo;
+    
+    public void Swap()
+    {
+        var original = gameObject;
+        var originalTransform = original.transform;
+        var newObject = Instantiate(swapsTo, originalTransform.position, originalTransform.rotation);
+        var rigidBody = newObject.GetComponent<Rigidbody>();
+        var originalRigidBody = original.GetComponent<Rigidbody>();
+        rigidBody.velocity = originalRigidBody.velocity;
+        rigidBody.angularVelocity = originalRigidBody.angularVelocity;
+        
+        Destroy(original);
+        
+    }
+}
